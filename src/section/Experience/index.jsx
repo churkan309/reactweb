28961e0c70fb6e20ef46b8017c905d7a932cea07
churkan309 <churkan.k@ku.th>
2026-02-15 16:42:02 +0700

    No more pls

--- src/section/Experience/index.jsx
+++ src/section/Experience/index.jsx
@@ -37,6 +37,31 @@ const Experience = () => {
                     <Tech isHighlight={isMountEnter} data={["React","Next","Tailwind"]} />
                 </div>
             </div>
+            <div className={`grid grid-cols-[25%_75%] rounded-md px-2 py-6 transition-all ${isMountEnter['exp2'] ? "bg-orange-200" : ""}`}
+                onMouseEnter={() => setIsMountEnter({ ['exp2']: true })}
+                onMouseLeave={() => setIsMountEnter({ ['exp2']: false })}
+            >
+                <div>
+                    <FromattedDate isHighlight={isMountEnter}>2023-2024</FromattedDate>
+                    <Picture picture={picEx2} title="Fullstack" />
+                </div>
+                <div className="grid gap-y-4">
+                    <div className={`text-primaryAccent ${isMountEnter['exp2'] ? "text-blue-950" : ""}`}>
+                        Fullstack
+                        <FontAwesomeIcon className={`text-xs -rotate-45 transition-all ${isMountEnter['exp2'] ? "translate-x-1 -translate-y-1" : ""}`} icon={faArrowRight} />
+                    </div>
+                    <div className="flex gap-4 text-lg">
+                        <a href="https://github.com/churkan309/reactweb" target="_blank" className="hover:scale-110 duration-300"><FontAwesomeIcon icon={faGithub} /></a>
+                        <a href="https://www.youtube.com/@ChurkanKLAIKRUEA" target="_blank" className="hover:scale-110 duration-300"><FontAwesomeIcon icon={faYoutube} /></a>
+                    </div>
+                    <div className="text-sm">Designing and developing</div>
+                    <div className="flex gap-4 text-sm">
+                        <div className={`rounded-md bg-primaryBase px-4 py-1 ${isMountEnter['exp2'] ? "text-primaryBg" : ""}`}>React</div>
+                        <div className={`rounded-md bg-primaryBase px-4 py-1 ${isMountEnter['exp2'] ? "text-primaryBg" : ""}`}>Next</div>
+                        <div className={`rounded-md bg-primaryBase px-4 py-1 ${isMountEnter['exp2'] ? "text-primaryBg" : ""}`}>Tailwind</div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
